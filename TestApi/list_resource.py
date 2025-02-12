db8a4cebd30d7671e3669f57e37efea8aea2133d
motorych88 <motorych88@mail.ru>
2025-02-12 15:05:51 +0300

    Добавил шаги allure в кейсы

--- TestApi/list_resource.py
+++ TestApi/list_resource.py
@@ -1,7 +1,7 @@
 import httpx
 from jsonschema import validate
-
 from Core.Contracts import RESOURCE_DATA_SCHEMA
+import allure
 
 URL = 'https://reqres.in/'
 LIST_RESOURCE = 'api/unknown'
@@ -9,22 +9,37 @@ SINGLE_RESOURCE = 'api/unknown/2'
 RESOURCE_NOT_FOUND = 'api/unknown/23'
 
 
+@allure.suite('Проверка запросов ресурса')
+@allure.title('Проверяем получение списка ресурсов')
 def test_list_resource():
-    response = httpx.get(URL + LIST_RESOURCE)
-    assert response.status_code == 200
+    with allure.step(f'Делаем запрос по адресу {URL + LIST_RESOURCE}'):
+        response = httpx.get(URL + LIST_RESOURCE)
+    with allure.step('Проверяем статус код'):
+        assert response.status_code == 200
     data = response.json()['data']
 
     for item in data:
-        validate(item, RESOURCE_DATA_SCHEMA)
-        assert item['color'].startswith('#')
+        with allure.step(f'Проверяем элемент из списка'):
+            validate(item, RESOURCE_DATA_SCHEMA)
+            with allure.step('Проверяем, что поле "color" начинается с #'):
+                assert item['color'].startswith('#')
 
 
+@allure.suite('Проверка запросов ресурса')
+@allure.title('Проверяем получения ресурса')
 def test_single_resource():
-    response = httpx.get(URL + SINGLE_RESOURCE)
-    assert response.status_code == 200
+    with allure.step(f'Делаем запрос по адресу {URL + SINGLE_RESOURCE}'):
+        response = httpx.get(URL + SINGLE_RESOURCE)
+    with allure.step('Проверяем статус код'):
+        assert response.status_code == 200
     data = response.json()['data']
-    assert data['color'].startswith('#')
+    with allure.step('Проверяем, что поле "color" начинается с #'):
+        assert data['color'].startswith('#')
 
+@allure.suite('Проверка запросов ресурса')
+@allure.title('Проверяем получение, если ресурса не существует')
 def test_resource_not_found():
-    response = httpx.get(URL + RESOURCE_NOT_FOUND)
-    assert response.status_code == 404
+    with allure.step(f'Делаем запрос по адресу {URL + RESOURCE_NOT_FOUND}'):
+        response = httpx.get(URL + RESOURCE_NOT_FOUND)
+    with allure.step('Проверяем статус код'):
+        assert response.status_code == 404
--- TestApi/list_users.py
+++ TestApi/list_users.py
@@ -1,7 +1,7 @@
 import httpx
 from jsonschema import validate
-
 from Core.Contracts import USER_DATA_SCHEMA
+import allure
 
 URL = 'https://reqres.in/'
 LIST_USERS = 'api/users?page=2'
@@ -11,25 +11,42 @@ SINGLE_USER = 'api/users/2'
 NOT_FOUND = 'api/users/23'
 
 
+@allure.suite('Проверка запросов данных пользователей')
+@allure.title('Проверяем получение списка пользователей')
 def test_list_users():
-    response = httpx.get(URL + LIST_USERS)
-    assert response.status_code == 200
+    with allure.step(f'Делаем запрос по адресу {URL + LIST_USERS}'):
+        response = httpx.get(URL + LIST_USERS)
+    with allure.step('Проверяем статус код'):
+        assert response.status_code == 200
     data = response.json()['data']
 
     for item in data:
-        validate(item, USER_DATA_SCHEMA)
-        assert item['email'].endswith(EMAIL_ENDS)
-        assert item['avatar'].endswith(str(item['id']) + AVATAR_ENDS)
+        with allure.step(f'Проверяем элемент из списка'):
+            validate(item, USER_DATA_SCHEMA)
+            with allure.step('Проверяем окончание email'):
+                assert item['email'].endswith(EMAIL_ENDS)
+            with allure.step('Проверяем, что id есть в поле "avatar"'):
+                assert item['avatar'].endswith(str(item['id']) + AVATAR_ENDS)
 
 
+@allure.suite('Проверка запросов данных пользователей')
+@allure.title('Проверяем получение пользователя')
 def test_single_user():
-    response = httpx.get(URL + SINGLE_USER)
-    assert response.status_code == 200
+    with allure.step(f'Делаем запрос по адресу {URL + SINGLE_USER}'):
+        response = httpx.get(URL + SINGLE_USER)
+    with allure.step('Проверяем статус код'):
+        assert response.status_code == 200
     data = response.json()['data']
-    assert data['email'].endswith(EMAIL_ENDS)
-    assert data['avatar'].endswith(str(data['id']) + AVATAR_ENDS)
+    with allure.step('Проверяем окончание email'):
+        assert data['email'].endswith(EMAIL_ENDS)
+    with allure.step('Проверяем, что id есть в поле "avatar"'):
+        assert data['avatar'].endswith(str(data['id']) + AVATAR_ENDS)
 
 
+@allure.suite('Проверка запросов данных пользователей')
+@allure.title('Проверяем получение, если пользователя не существует')
 def test_not_found():
-    response = httpx.get(URL + NOT_FOUND)
-    assert response.status_code == 404
+    with allure.step(f'Делаем запрос по адресу {URL + NOT_FOUND}'):
+        response = httpx.get(URL + NOT_FOUND)
+    with allure.step('Проверяем статус код'):
+        assert response.status_code == 404
